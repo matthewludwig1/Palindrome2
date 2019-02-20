3944d407e67aca912bf052472cbf01b21d5ea59b
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-02-20 16:57:47 -0500

    Update Palindrome.java

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,48 +1,60 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  Matthew Ludwig
+  20 February 2019
+  This is a modified program which counts the number of palindromes in a sentence
  */
 
 package palindrome;
 
 /**
  *
- * @author rkaune
+ * @author @malud0519
  */
 import javax.swing.*;
 
 public class Palindrome {
 
     public static void main(String[] args) {
-        String word, backwardsWord;
+        String word, backwardsWord, x;
         int pos;
-        
-
+        char ch;
+        x = "";
         //get the word from the user
         word = JOptionPane.showInputDialog(
                 "Words that are the same forwards and backwards are called palindromes.\n"
-                + "This program determines if a words is a palindrome.\n\n"
+                + "This program determines if a word, or sentence is a palindrome.\n\n"
                 + "Enter a sentence (do not include a punctuation mark):");
-        word.split(" ");
-        String [] words = word.split(" "); // Splits the words in the sentence
-        
-        //Reverses word
-     for (int i = 0; i < words.length; i++) {
+         word.split(" ");
+         String [] words = word.split(" "); // Splits the words in the sentence
           backwardsWord = ""; 
-         for (pos = word.length() - 1; pos >= 0; pos--) {
-          backwardsWord += words[i];
-         // if
-        }
-     }
-        
+        //Reverses word
+        // backwardsWord += words[i];
+            
+        for (int i = 0; i < words.length; i++) 
+        {
+           ch = word.charAt(i);
+           if (ch != ' ') 
+           {
+               backwardsWord += ch;
+               x += ch;
+           }
+                else {
+                    if (backwardsWord.equalsIgnoreCase(x))
+                    {
+                        JOptionPane.showMessageDialog(null, "The words " + backwardsWord);
+                    }
+                    backwardsWord = ""; //clears
+                    x = ""; 
+                }   
         //Compare the words
-          if (word.equalsIgnoreCase(backwardsWord)) {
+         /* if (word.equalsIgnoreCase(backwardsWord)) {
             JOptionPane.showMessageDialog(null, word + " in reverse is " + backwardsWord
                     + ".\nIt IS a palindrome.");
-        } else {
+        }  
+        else {
             JOptionPane.showMessageDialog(null, word + " in reverse is " + backwardsWord
                     + ".\nIt IS NOT a palindrome.");
-        }
+        } */
     }
+}
 }
