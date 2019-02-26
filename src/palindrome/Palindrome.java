9158613bf9ac7b267f8e18a59282e9eec287cee6
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-02-26 10:49:10 -0500

    Update Palindrome.java

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -15,46 +15,27 @@ import javax.swing.*;
 public class Palindrome {
 
     public static void main(String[] args) {
-        String word, backwardsWord, x;
-        int pos;
-        char ch;
-        x = "";
+        String word, backwardsWord;
+        int pos;      
         //get the word from the user
         word = JOptionPane.showInputDialog(
                 "Words that are the same forwards and backwards are called palindromes.\n"
                 + "This program determines if a word, or sentence is a palindrome.\n\n"
                 + "Enter a sentence (do not include a punctuation mark):");
-         word.split(" ");
-         String [] words = word.split(" "); // Splits the words in the sentence
-          backwardsWord = ""; 
-        //Reverses word
-        // backwardsWord += words[i];
-            
+        word.split(" ");
+        String [] words = word.split(" "); // Splits the words in the sentence
+        int num = 0;
+        //Reverses word 
         for (int i = 0; i < words.length; i++) 
         {
-           ch = word.charAt(i);
-           if (ch != ' ') 
-           {
-               backwardsWord += ch;
-               x += ch;
-           }
-                else {
-                    if (backwardsWord.equalsIgnoreCase(x))
-                    {
-                        JOptionPane.showMessageDialog(null, "The words " + backwardsWord);
-                    }
-                    backwardsWord = ""; //clears
-                    x = ""; 
-                }   
-        //Compare the words
-         /* if (word.equalsIgnoreCase(backwardsWord)) {
-            JOptionPane.showMessageDialog(null, word + " in reverse is " + backwardsWord
-                    + ".\nIt IS a palindrome.");
-        }  
-        else {
-            JOptionPane.showMessageDialog(null, word + " in reverse is " + backwardsWord
-                    + ".\nIt IS NOT a palindrome.");
-        } */
+            backwardsWord = ""; 
+            for (pos = words[i].length() - 1; pos >= 0; pos--) {
+                backwardsWord += words[i].charAt(pos); //reverses word
+            }
+            if (words[i].equalsIgnoreCase(backwardsWord)) {
+               num ++;
+            }
+        }    
+         JOptionPane.showMessageDialog(null, "There are " + num + " palindromes in this sentence" );
     }
 }
-
